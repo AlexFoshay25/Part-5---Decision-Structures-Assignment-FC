30c08fd20df9995269639fab631830641c0156aa
Alex Foshay <FoshAlex157@GoTVDSB.ca>
2021-03-02 12:21:37 -0500

    Add project files.

---
+++ Part 5 - Decision Structures Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Part_5___Decision_Structures_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
